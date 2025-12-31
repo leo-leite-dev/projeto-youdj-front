910cbf0eaca4c825235b4b3f89728c9a86b66c9f
Leonardo <leopassos.leite@gmail.com>
2025-12-31 00:26:56 -0300

    :wrench: configurar substituição do environment.prod para deploy no Vercel

--- src/app/features/environments/environments.prod.ts
+++ src/app/features/environments/environments.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://projeto-gamenewsboard-back.fly.dev/api',
-  apiRedirectBaseUrl: 'https://projeto-gamenewsboard-back.fly.dev',
-  uploadsBaseUrl: 'https://xkfahqlwbxotkggjfams.supabase.co/storage/v1/object/public/uploads/'
+  apiBaseUrl: 'https://projeto-youdj-back.onrender.com',
 };
